de55034e8c7389a51edfc271aef19a0a78371f96
“Mardie328” <tolayinka328@gmail.com>
2022-04-17 15:36:27 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int ch;
+	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
